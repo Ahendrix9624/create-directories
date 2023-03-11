6b2589f9de3be61ad8047a3a9ca26c875bcf5cbe
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:52:37 -0700

    Update create-directory.py

--- create-directory.py
+++ create-directory.py
@@ -1,38 +1,14 @@
-#!/usr/bin/env python3
-#
-#  [Program]
-#
-#  Creates X amount of directory's
-#
-#  [Author]
-#
-#  Drew, https://github.com/Ahendrix9624/
-#
-#  [License]
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#  See 'LICENSE' for more information.
-
-# import requests HTTP Library module, import json module
+"""
+USAGE - This code creates a folder with a specific name for each number in the range of 1 to 100. 
+        It checks if a folder with the name already exists, and if not, it creates one using 
+        the 'os' module.
+        
+AUTHOR - https://github.com/Ahendrix9624/
+"""
 
 import os
 
-numbers = list(range(1, 101))
-
-for number in numbers:
-    path = "day" + str(number)
+for number in range(1, 101):
+    path = f"day{number}"
     if not os.path.exists(path):
         os.mkdir(path)
